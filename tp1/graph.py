2e685918f7c0b29ced0868839eb1085a2eb355d4
Italo Leal <italoleallanasantos@gmail.com>
2025-09-29 16:14:23 -0300

    Refactor Graph class and update main.py: missing graph methods implementation

--- tp1/graph.py
+++ tp1/graph.py
@@ -1,4 +1,29 @@
 class Graph:
-    def __init__(self, vertices):
-        self.V = vertices
-        self.graph = {}
+    def __init__(self, vertex_count: int): 
+        self.vertex_count: int = vertex_count 
+        self.edges: list[tuple[int, int, int]] = []
+        self.graph: dict[int, list[tuple[int, int, int]]] = { i: [] for i in range(1, vertex_count + 1) }
+
+    def __str__(self):
+        # Escrever uma forma melhor de representar o grafo:
+        result = f"Grafo({self.vertex_count} vertices e {len(self.edges)} arestas):\n"
+        for from_vertex, to_vertex, length in self.edges:
+            result += f"  {from_vertex} <--({length})--> {to_vertex}\n"
+        return result
+        
+
+    # Por o grafo ser bidirecionado, a função add_edge já adiciona as duas direções
+    def add_edge(self, from_vertex: int, to_vertex: int, length: int):
+        self.edges.append((from_vertex, to_vertex, length))
+        self.graph[from_vertex].append((to_vertex, length))
+        self.graph[to_vertex].append((from_vertex, length))
+
+    def dijkstra(self, start: int, end: int) -> list[int]:
+        return [-1]
+
+    def find_minimal_streets(self, start: int, end: int) -> set[int]:
+        return {-1}
+
+    def find_critical_streets(self, start: int, end: int) -> set[int]:
+        return {-1}
+
--- tp1/main.py
+++ tp1/main.py
@@ -1,6 +1,7 @@
 import os
 import sys
-import graph
+from graph import Graph
+
 
 def get_filepath(filename: str) -> str:
     candidate_paths = [
@@ -25,57 +26,59 @@ def main(argv=sys.argv) -> str:
     filepath = get_filepath(argv[1]) 
 
     with open(filepath, "r") as file:
-        data = file.read()
+        lines = file.readlines()
 
-    print(data)
-    return data
+    # Só a primeira linha é lida separadamente para pegar só número de vértices
+    header = lines[0].strip().split()
+    vertex_count = int(header[0])
 
+    g: Graph = Graph(vertex_count)
+
+    for line in lines[1:]:
+        data_str: list[str] = line.strip().split()
+        data_int: list[int] = [int(x) for x in data_str]
+        vertex_a, vertex_b, street_length = data_int
+        g.add_edge(vertex_a, vertex_b, street_length)
+
+
+    # print(g)
+
+    ''' # Parte 1
+    Praça central continue sendo de rápido acesos para o principal parque ecológico da cidade.
+    Qual a distância mínima da praça (região 1) para o parque (região N), considerando todas as ruas da cidade em funcionamento?
+    - Saída: String "Parte 1: ", seguida de distancia_minima_da_regiao_1_para_regiao_N (inteiro). É garantido que há >=1 caminho entre as regiões 1 e N.
+    - RESUMO: Imprimir a menor distância entre 1 e N. '''
+
+    path: list[int] = g.dijkstra(1, vertex_count)
+    result = f"Parte 1: {len(path)}\n"
+
+    ''' # Parte 2
+    Identificar todas as ruas que participam de pelo menos uma rota de menor distância da praça para o parque.
+    Essas ruas são candidatas importantes (utilizam com mais frequência).
+    - Saída: String "Parte 2: ", seguida dos índices das ruas que participam de pelo menos um caminho mínimo entre a região 1 e N.
+    - RESUMO: Encontrar todos os caminhos mínimos e imprimir as (índices) arestas que participam desses caminhos. '''
+
+    minimal_streets: set[int] = g.find_minimal_streets(1, vertex_count)
+    result += f"Parte 2: {sorted(minimal_streets)}\n"
+
+    ''' # Parte 3
+    Identificar quais são as ruas críticas da cidade. Essas ruas não podem ser destruidas, visto que caso destruidas, fariam com que a menor distâcia
+    entre 1 e N aumentasse, ou tornaria o acesso impossível.
+    - Saída: String "Parte 3: ", seguida dos índices das ruas críticas, em ordem crescente. Os índices são definidos pela ordem 
+    em que as ruas foram fornecidas na entrada, começando em 1. Se não houver nenhuma, imprima -1.
+    RESUMO: Encontrar todas as (índices) das arestas que, se removidas, fariam com que a distância mínima entre 1 e N aumentasse. "R1 R2 ... RN" '''
+
+    critical_streets: set[int] = g.find_critical_streets(1, vertex_count)
+    if len(critical_streets) == 0:
+        result += "Parte 3: -1"
+    else:
+        result += f"Parte 3: {sorted(critical_streets)}"
+
+    return result
 
 if __name__ == "__main__":
-    main()
-
-
-
-'''
-Somatório possui N regiões (vértices 1-N) e M ruas (arestas).
-Todas as ruas são de duplos sentido, e possuem uma distância associada.
-3 Perguntas:
-
-
-# Parte 1
-Praça central continue sendo de rápido acesos para o principal parque ecológico da cidade.
-Qual a distância mínima da praça (região 1) para o parque (região N), considerando todas as ruas da cidade em funcionamento?
-- Saída: String "Parte 1: ", seguida de distancia_minima_da_regiao_1_para_regiao_N (inteiro). É garantido que há >=1 caminho entre as regiões 1 e N.
-- RESUMO: Imprimir a menor distância entre 1 e N. 
-
-# Parte 2
-Identificar todas as ruas que participam de pelo menos uma rota de menor distância da praça para o parque.
-Essas ruas são candidatas importantes (utilizam com mais frequência).
-- Saída: String "Parte 2: ", seguida dos índices das ruas que participam de pelo menos um caminho mínimo entre a região 1 e N.
-- RESUMO: Encontrar todos os caminhos mínimos e imprimir as arestas que participam desses caminhos.
-
-# Parte 3
-Identificar quais são as ruas críticas da cidade. Essas ruas não podem ser destruidas, visto que caso destruidas, fariam com que a menor distâcia
-entre 1 e N aumentasse, ou tornaria o acesso impossível.
-- Saída: String "Parte 3: ", seguida dos índices das ruas críticas, em ordem crescente. Os índices são definidos pela ordem 
-em que as ruas foram fornecidas na entrada, começando em 1. Se não houver nenhuma, imprima -1.
-RESUMO: Encontrar todas as arestas que, se removidas, fariam com que a distância mínima entre 1 e N aumentasse.
-'''
-
+    # Eu implementei uma classe de teste (tests.py), desta forma, o código funciona em ambos os casos:
+    # "python main.py <inputfile>" ou "python tests.py"
     
-'''
-
-Entrada:
-N M         # N regiões (vértices) e M ruas (arestas)
-A1 B1 C1    # Ai e Bi regiões que rua i conecta, Ci comprimento da rua i
-A2 B2 C2    
-...
-AM BM CM
-
-
-Saída:
-Parte 1: D              # a distância encontrada na primeira tarefa.
-Parte 2: R1 R2 ... RN   # e os índices das ruas encontradas na segunda tarefa, separados por espaço.
-Parte 3: R1 R2 ... RN   # e os índices das ruas encontradas na terceira tarefa, separados por espaço.
-
-'''
+    output = main()
+    print(output)
