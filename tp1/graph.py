823569e96f80707e833608dd8a73c1e8a77e74a8
Italo Leal <italoleallanasantos@gmail.com>
2025-10-10 18:42:20 -0300

    Working Version: Add comments + clean code

--- tp1/graph.py
+++ tp1/graph.py
@@ -1,13 +1,15 @@
 from heapq import heappop, heappush
 
 class Graph:
-    def __init__(self, vertex_count: int) -> None: 
+    def __init__(self, vertex_count: int, edges_count: int) -> None: 
         self.vertex_count: int = vertex_count
-        self.edge_count: int = 0
+        self.edge_count: int = edges_count
+
         # GRAFO = {vertice1: [(vertice2, distancia), (vertice3, distancia), ...]; ...}
         self.graph: dict[int, list[tuple[int, int]]] = { i: [] for i in range(1, vertex_count + 1) }
         # ARESTAS = [(aresta1, vertice1, vertice2, distancia), (aresta2, vertice1, vertice3, distancia), ...]
         self.edges: list[tuple[int, int, int, int]] = []
+        # Arestas mínimas (entre 1 e N) para evitar chamadas duplicadas
         self.minimal_edges: list[tuple[int, int, int, int]] = []
 
 
@@ -18,38 +20,33 @@ class Graph:
         return result
         
     
-    def add_edge(self, from_vertex: int, to_vertex: int, length: int) -> None:
+    def add_edge(self, index: int, from_vertex: int, to_vertex: int, length: int) -> None:
+        # Adiciona uma aresta ao grafo (tanto na lista de arestas quanto no dicionário do grafo)
         self.edge_count += 1
-        self.edges.append([self.edge_count, from_vertex, to_vertex, length])
+        self.edges.append([index, from_vertex, to_vertex, length])
         # O grafo é bidirecionado, com a mesma distancia p/ os 2 lados (ruas)
         self.graph[from_vertex].append((to_vertex, length))
         self.graph[to_vertex].append((from_vertex, length))
 
-    
-    def get_edge_count(self) -> int:
-        return len(self.edges)
-
-    
-    def get_vertex_count(self) -> int:
-        return len(self.graph)
-    
 
     def _dfs(self, initial_vertex: int) -> set:
+        # Busca em profundidade para encontrar todos os vértices alcançáveis a partir de um vértice inicial
         stack: list[int] = [initial_vertex]
         visited: set = set()
         
         while stack:
-            vertex: int = stack.pop()
+            vertex: int = stack.pop() # Pega o último vértice adicionado
             if vertex not in visited:
                 visited.add(vertex)
-                for neighbor, length in self.graph[vertex]:
+                for neighbor, length in self.graph[vertex]: # Para todos os vértices vizinhos
                     if neighbor not in visited:
                         stack.append(neighbor)
         return visited
 
 
     def _dijkstra(self, initial_vertex: int) -> list:
-        heap: list[int, int] = [(0, initial_vertex)] # (distância_acumulada, indice_vértice)
+        # Algoritmo padrão que retorna uma lista com a menor distância de um vértice inicial a todos os outros
+        heap: list[(int, int)] = [(0, initial_vertex)] # (distância_acumulada, indice_vértice)
         parent: list[int] = [-1] * (self.vertex_count + 1)
         distance: list[int] = [float('inf')] * (self.vertex_count + 1)
         distance[initial_vertex] = 0
@@ -67,35 +64,38 @@ class Graph:
 
 
     def get_minimal_distance(self, start: int, end: int) -> int:
+        # Obtém a distância mínima entre dois vértices
         distance: list[int] = self._dijkstra(initial_vertex = start)
         return distance[end]
 
     
     def find_minimal_edges(self, start: int, end: int) -> list:
+        # Encontra todas as arestas que pertencem a pelo menos um caminho mínimo entre dois vértices
         self.minimal_edges = []
         distance_from_start: list[int] = self._dijkstra(start)
         distance_from_end: list[int] = self._dijkstra(end)
         minimal_distance: int = distance_from_start[end]
 
+        # Verifica todas as arestas para ver se pertencem ao caminho mínimo
         for index, u, v, length in self.edges:
             cond1: bool = distance_from_start[u] + length + distance_from_end[v] == minimal_distance
             cond2: bool = distance_from_start[v] + length + distance_from_end[u] == minimal_distance
             if cond1 or cond2:
                 self.minimal_edges.append([index, u, v, length])
-        return self.minimal_edges
+        return sorted(self.minimal_edges)
     
     
     def find_critical_edges(self, start: int, end: int) -> set:
+        # Encontra todas as arestas críticas, ou seja, que se removidas desconectam o caminho mínimo entre dois vértices
         if not self.minimal_edges: 
             self.find_minimal_edges(start, end) # Usado para evitar chamadas duplicadas
           
         critical_streets: list[int] = []
-        minimal_graph: Graph = Graph(self.vertex_count)
 
+        # Cria um outro grafo com apenas as arestas mínimas
+        minimal_graph: Graph = Graph(self.vertex_count, len(self.minimal_edges))
         for index, u, v, length in self.minimal_edges:
-            minimal_graph.graph[u].append((v, length))
-            minimal_graph.graph[v].append((u, length))
-            minimal_graph.edges.append((index, u, v, length))
+            minimal_graph.add_edge(index, u, v, length)
 
         for index, u, v, length in minimal_graph.edges:
             # Remover a aresta
@@ -108,4 +108,4 @@ class Graph:
             # Adicionar a aresta novamente
             minimal_graph.graph[u].append((v, length))
             minimal_graph.graph[v].append((u, length))
-        return critical_streets
+        return sorted(critical_streets)
--- tp1/main.py
+++ tp1/main.py
@@ -29,33 +29,42 @@ def main(argv=sys.argv) -> str:
     else:
         raise ValueError("Please provide an input file (argument or stdin).")
 
-    # Só a primeira linha é lida separadamente para pegar só o número de vértices
-    header = lines[0].strip().split()
-    vertex_count = int(header[0]) # Quantidade de vértices = ID do Parque
-    g: Graph = Graph(vertex_count)
-    for line in lines[1:]:
+    # Só a primeira linha é lida separadamente para pegar o número de vértices e arestas
+    header: list[str] = lines[0].strip().split()
+    vertex_count: int = int(header[0])
+    edges_count: int = int(header[1])
+    g: Graph = Graph(vertex_count, edges_count)
+
+    # Lê as linhas da entrada, e adiciona as arestas (com o indice certo) ao grafo
+    for index_edge, line in enumerate(lines[1:], start=1): 
         data: list[str] = line.strip().split()
         if not data:
             continue
         edge: list[int] = [int(x) for x in data]
         vertex_a, vertex_b, street_length = edge
-        g.add_edge(vertex_a, vertex_b, street_length)
+        g.add_edge(index_edge, vertex_a, vertex_b, street_length)
 
-    distance: int = g.get_minimal_distance(1, vertex_count)
+    # Constantes armazenando os índices dos vértices de início e fim
+    PRACA: int = 1
+    PARQUE: int = vertex_count
+
+    # Chamadas das funções que resolvem o problema
+    distance: int = g.get_minimal_distance(PRACA, PARQUE)
+    minimal_streets: list[tuple] = g.find_minimal_edges(PRACA, PARQUE)
+    critical_streets: set[int] = g.find_critical_edges(PRACA, PARQUE)
+
+    # Formatação da saída
+    minimal_streets_str = " ".join(str(index) for index, u, v, length in minimal_streets)
+    critical_streets_str = " ".join(str(street) for street in critical_streets) if critical_streets else "-1"
     result = f"Parte 1: {distance}\n"
-
-    minimal_streets: list[tuple] = g.find_minimal_edges(1, vertex_count)
-    result += f"Parte 2: " + " ".join(str(index) for index, u, v, length in sorted(minimal_streets)) + "\n"
-
-    critical_streets: set[int] = g.find_critical_edges(1, vertex_count)
-    if len(critical_streets) == 0: 
-        result += "Parte 3: -1"
-    else: 
-        result += f"Parte 3: " + " ".join(str(street) for street in sorted(critical_streets))
+    result += f"Parte 2: {minimal_streets_str}\n"
+    result += f"Parte 3: {critical_streets_str}"
 
     return result 
 
 
 if __name__ == "__main__":
+    # A saída só é impressa no VPL. Nos testes locais (tests.py), o return result é usado para comparação
     output = main()
-    print(output) # Para validação no VPL
+    print(output) 
+    
