92d307c90a66bc1325b9097c13ac4adef36ac6c7
Italo Leal <italoleallanasantos@gmail.com>
2025-10-09 22:57:56 -0300

    Feat: Add total time to tests

--- tp1/tests.py
+++ tp1/tests.py
@@ -1,5 +1,6 @@
 import sys
 import os
+import time
 sys.path.append(os.path.abspath('D:\\Code\\alg1\\tp1'))
 
 import main
@@ -15,14 +16,24 @@ WHITE = '\033[97m'
 def assertionErrorText(exp: string, act: string) -> string:
     return f"""{BOLD}{GREEN}\nExpected:\n{RESET}{exp}{BOLD}{RED}\nActual:\n{RESET}{act}\n"""
 
+header: string = \
+    f"{BOLD}{CYAN}"\
+    f"========================================\n" f"#{WHITE}"\
+    f"         PY_UNIT_ASSERT REPORT        "     f"{CYAN}#\n"\
+    f"========================================\n" f"{RESET}"
 
 tests_path = ".\\tests\\"
+
 tests = [ (i, f"{tests_path}in{i:02}.txt", f"{tests_path}out{i:02}.txt")
     for i in range(1, 14) ]
 
 errors = []
 
 if __name__ == "__main__":
+    start_time: float = time.time()
+
+    print(header)
+
     for i, in_file, out_file in tests:
         print(f"{BOLD}{CYAN}Running test {i}/{len(tests)}...{RESET}")
         try:
@@ -33,14 +44,11 @@ if __name__ == "__main__":
         except AssertionError as e:
             errors.append(f"{BOLD}{CYAN}Test {i} failed:{e}{RESET}")
 
-
-    print(f"{BOLD}{CYAN}"
-          f"========================================\n" f"#{WHITE}"
-          f"         PY_UNIT_ASSERT REPORT        "     f"{CYAN}#\n"
-          f"========================================\n" f"{RESET}")
-
     if errors:
         for err in errors:
             print(err)
     else:
-        print(f"{BOLD}{GREEN}All tests passed!{RESET}")
+        print(f"\n{BOLD}{GREEN}All tests passed!{RESET}")
+
+    end_time: float = time.time()
+    print(f"{BOLD}{CYAN}Total time: {end_time - start_time:.3f} seconds{RESET}")
