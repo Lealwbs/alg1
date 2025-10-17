0aef6beca21514f59d8b66deb7289657b8649ec8
Ítalo Leal <italoleallanasantos@gmail.com>
2025-10-17 19:46:13 -0300

    Add fib iterations method inside class

--- algorithms/fibonacci.py
+++ algorithms/fibonacci.py
@@ -1,12 +1,5 @@
-from math import sqrt
+from math import sqrt, pow
 from time import time
-
-def get_time(func, param: int) -> float:
-    start_time: float = time()
-    func(param)
-    end_time: float = time()
-    result: float = (end_time - start_time)
-    return result
 
 class Fibonacci:
     def __init__(self):
@@ -35,7 +28,7 @@ class Fibonacci:
         if n == 1: return 1
         a: float = (1 + sqrt(5))/2 
         b: float = (1 - sqrt(5))/2
-        result: float = (a**n + b**n) / sqrt(5) 
+        result: float = (pow(a, n) + pow(b, n)) / sqrt(5) 
         return round(result)
 
     def fib_cache(self, n: int) -> int:
@@ -44,17 +37,27 @@ class Fibonacci:
                 self.cache[i] = self.cache[i-1] + self.cache[i-2] 
                 self.saved += 1
         return self.cache[n]
+    
+    def print_fib_iterations(self, start: int, end: int) -> None:
+        for i in range(start, end+1):
+            print(f"Iteração {i:>3}:")
+            print(f"ITERA: {self.get_time(self.fib_iterative,   i):.6f}") 
+            print(f"RECUR: {self.get_time(self.fib_recursive,   i):.6f}") 
+            print(f"EXPON: {self.get_time(self.fib_exponential, i):.6f}")
+            print(f"CACHE: {self.get_time(self.fib_cache,       i):.6f}")
+            print()
+
+    def get_time(self, func, param: int) -> float:
+        start_time: float = time()
+        func(param)
+        end_time: float = time()
+        result: float = (end_time - start_time)
+        return result
 
 
 if __name__ == "__main__":
     F: Fibonacci = Fibonacci()
-    num: int = 35
+    F.print_fib_iterations(0, 20)
 
-    for i in range(num):
-        print(f"Iteração {i:>3}:")
-        print(f"ITERA: {get_time(F.fib_iterative,   i):.6f}") 
-        print(f"RECUR: {get_time(F.fib_recursive,   i):.6f}") 
-        print(f"EXPON: {get_time(F.fib_exponential, i):.6f}")
-        print(f"CACHE: {get_time(F.fib_cache,       i):.6f}")
-        print()
+
 
