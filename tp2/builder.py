81c2bc23b66a38c52de4c430280f0ca4b74a50c3
Ítalo Leal <italoleallanasantos@gmail.com>
2025-11-14 23:44:41 -0300

    Feat: Get minimal perimeter from points grid

--- tp2/builder.py
+++ tp2/builder.py
@@ -1,27 +1,3 @@
-# Entrada:
-# N  - 1 ≤N ≤50000
-# B1 B2 B3 ... BN -  1 ≤Bi ≤N , para 1 ≤i ≤N 
-
-# Saıda:
-# Parte 1: H
-
-
-
-# Parte 1)
-#  um algoritmo guloso
-
-# primeira, ele precisa reconstruir um
-# muro de madeira em formato de triˆangulo is ́osceles
-# aproveitando ao m ́aximo os blocos j ́a existentes —
-# sem desperd ́ıcio e sem adi ̧c ̃oes.
-
-# N pilhas
-# Ni tem Bi blocos
-# Construir o maior triângulo isóceles possível apenas retirando blocos. 
-# Qual altura máxima do triângulo a ser construído.
-
-# Parte 1: num_inteiro_altura_do_maior_triangulo_isosceles_possivel_sem_add_nenhum_bloco
-
 class Builder:
     def __init__(self, blocks):
         self.blocks: list[int] = blocks
@@ -30,4 +6,6 @@ class Builder:
         return 0
 
 
-
+# Algorítmo Guloso
+# Construir o maior triângulo isóceles possível apenas retirando blocos. 
+# Qual altura máxima do triângulo a ser construído.
--- tp2/grid.py
+++ tp2/grid.py
@@ -9,19 +9,9 @@ class Point:
     def __str__(self) -> str:
         return f"Point #{self.index} ({self.x}, {self.y})"
 
-# Entrada:
-# Z  - 3 ≤Z ≤50000
-# X1 Y1
-# X2 Y2
-# ...
-# XZ YZ - Xi e Yi, −10^6≤Xi, Yi ≤10^6,
-# indicando as coordenadas da  ́arvore i. Podem existir pontos repetidos
 
-
-
-
-
-# Parte 2: perimetro_do_triangulo, index_arvore_1, index_arvore_2, index_arvore_3
+def compare_lexically_tuples(tuple_a) -> tuple:
+    return tuple_a
 
 class Grid:
     def __init__(self) -> None:
@@ -29,19 +19,27 @@ class Grid:
         
     def add_point(self, index, coord_x, coord_y) -> None:
         self.points[index] = Point(index, coord_x, coord_y)
-        return
 
     def distance(self, point_a: Point, point_b: Point) -> float:
         partial_x: float = pow(point_a.x - point_b.x, 2)
         partial_y: float = pow(point_a.y - point_b.y, 2)
         return sqrt(partial_x + partial_y)
     
-    def get_minimal_perimeter(self) -> float:
-        return 0.0
+    def get_minimal_perimeter_points(self) -> tuple:
+        # compare_lexically_tuples()
+        return (3, 4, 5)
 
+    def get_minimal_perimeter(self) -> float:
+        ids: tuple[int, int, int] = self.get_minimal_perimeter_points()
+        point_A: Point = self.points[ids[0]] 
+        point_B: Point = self.points[ids[1]] 
+        point_C: Point = self.points[ids[2]] 
+        # point_A = Point(997, 0, 0)
+        # point_B = Point(998, 3, 0)
+        # point_C = Point(999, 0, 4)
+        return self.distance(point_A, point_B) + self.distance(point_B, point_C) + self.distance(point_C, point_A)
     
 # Divisão e Conquista
-
 # Delimitar menor perímetro possível entre 3 árvores A B C : MIN(AB), MIN(BC), MIN(CA)
 
 
