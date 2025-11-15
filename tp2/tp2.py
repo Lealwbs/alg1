f2afea0a3f47f1fde299988412ea321c0f08dbbb
Ítalo Leal <italoleallanasantos@gmail.com>
2025-11-14 21:30:59 -0300

    Feat: Start point of tp2

---
+++ tp2/tp2.py
@@ -0,0 +1,73 @@
+import os
+import sys
+from tp2.trianguland import IsoscelesWallBuilder, MinPerimeterFence
+
+# Usado para caso os arquivos de teste .txt estejam em uma pasta diferente do main.py
+def get_filepath(filename: str) -> str:
+    candidate_paths = [
+        filename,
+        os.path.join("tp2", filename),
+        os.path.join("tp2", "tests", filename)
+    ]
+
+    for path in candidate_paths:
+        if os.path.exists(path):
+            return path
+
+    raise FileNotFoundError(f"File not found in any location: {filename}")
+
+
+def main(argv=sys.argv) -> str:
+    # Adaptação para ler tanto do arquivo (argumento) quanto do stdin (no vpl)
+    if len(argv) > 1: 
+        filepath = get_filepath(argv[1]) 
+        with open(filepath, "r") as file:
+            lines = file.readlines()
+    elif not sys.stdin.isatty():
+        lines = sys.stdin.readlines()
+    else:
+        raise ValueError("Please provide an input file (argument or stdin).")
+
+    # # Só a primeira linha é lida separadamente para pegar o número de vértices e arestas
+    # header: list[str] = lines[0].strip().split()
+    # vertex_count: int = int(header[0])
+    # edges_count: int = int(header[1])
+    # g: Graph = Graph(vertex_count, edges_count)
+    I: IsoscelesWallBuilder = IsoscelesWallBuilder()
+    F: MinPerimeterFence = MinPerimeterFence()
+
+    # # Lê as linhas da entrada, e adiciona as arestas (com o indice certo) ao grafo
+    # for index_edge, line in enumerate(lines[1:], start=1): 
+    #     data: list[str] = line.strip().split()
+    #     if not data:
+    #         continue
+    #     edge: list[int] = [int(x) for x in data]
+    #     vertex_a, vertex_b, street_length = edge
+    #     g.add_edge(index_edge, vertex_a, vertex_b, street_length)
+
+    # # Constantes armazenando os índices dos vértices de início e fim
+    # PRACA: int = 1
+    # PARQUE: int = vertex_count
+
+    # # Chamadas das funções que resolvem o problema
+    # distance: int = g.get_minimal_distance(PRACA, PARQUE)
+    # minimal_streets: list[tuple] = g.find_minimal_edges(PRACA, PARQUE)
+    # critical_streets: set[int] = g.find_critical_edges(PRACA, PARQUE)
+
+    # # Formatação da saída
+    # minimal_streets_str = " ".join(str(index) for index, u, v, length in minimal_streets)
+    # critical_streets_str = " ".join(str(street) for street in critical_streets) if critical_streets else "-1"
+    result = f"Parte 1: \n"
+    # result += f"Parte 2: {minimal_streets_str}\n"
+    # result += f"Parte 3: {critical_streets_str}"
+
+    return result 
+
+
+if __name__ == "__main__":
+    # A saída só é impressa no VPL. Nos testes locais (tests.py), o return result é usado para comparação
+    output: str = main()
+    print(output) 
+    
+
+
---
+++ tp2/trianguland.py
@@ -0,0 +1,72 @@
+# Entrada:
+# N  - 1 ≤N ≤50000
+# B1 B2 B3 ... BN -  1 ≤Bi ≤N , para 1 ≤i ≤N 
+
+# Saıda:
+# Parte 1: H
+
+
+
+# Parte 1)
+#  um algoritmo guloso
+
+# primeira, ele precisa reconstruir um
+# muro de madeira em formato de triˆangulo is ́osceles
+# aproveitando ao m ́aximo os blocos j ́a existentes —
+# sem desperd ́ıcio e sem adi ̧c ̃oes.
+
+# N pilhas
+# Ni tem Bi blocos
+# Construir o maior triângulo isóceles possível apenas retirando blocos. 
+# Qual altura máxima do triângulo a ser construído.
+
+# Parte 1: num_inteiro_altura_do_maior_triangulo_isosceles_possivel_sem_add_nenhum_bloco
+
+class IsoscelesWallBuilder:
+    pass
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Entrada:
+# Z  - 3 ≤Z ≤50000
+# X1 Y1
+# X2 Y2
+# ...
+# XZ YZ - Xi e Yi, −10^6≤Xi, Yi ≤10^6,
+# indicando as coordenadas da  ́arvore i. Podem existir pontos repetidos
+
+# Saıda:
+# Parte 2: P A1 A2 A3  (P: com exatamente quatro casas decimais, indices, ordem crescente, lexicograficamente menor)
+
+# Parte 2)
+# divis ̃ao e conquista
+
+# Na segunda, ele deseja delimitar uma  ́area triangular entre trˆes  ́arvores
+# escolhidas em um parque da cidade, de forma que o per ́ımetro seja o menor poss ́ıvel,garantindo
+# economia de recursos e preserva ̧c ̃ao ambiental na instala ̧c ̃ao da nova cerca ecol ́ogica
+
+# Z árvores (x,y)
+
+# Escolher 3 árvores para cercar, triangulo, o perímetro deve ser o menor possível.
+
+# A B C : MIN(AB), MIN(BC), MIN(CA)
+
+# Parte 2: perimetro_do_triangulo, index_arvore_1, index_arvore_2, index_arvore_3
+
+class MinPerimeterFence:
+    pass
